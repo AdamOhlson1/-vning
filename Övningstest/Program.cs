13968c796f4dab7ef5d3f66884c5f0e63f55fd13
Adam Ohlson <adam.ohlson0531@gmail.com>
2024-01-08 11:33:02 +0100

    first commit

---
+++ Övningstest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class StringManipulator
+{
+    public string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public int CountVowels(string input)
+    {
+        return input.Count(c => "aeiouAEIOU".Contains(c));
+    }
+
+    public string ConcatenateStrings(string str1, string str2)
+    {
+        return $"{str1} {str2}";
+    }
+}
